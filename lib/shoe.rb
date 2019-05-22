bbfe3474f9739b4995cff1b4fbd4ada663c780a3
Denali Balser <denali.balser167@gmail.com>
2019-05-22 00:48:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,17 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition, :cobble
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
   end
 
+  def cobble=(cobble)
+    @cobble = cobble
+    @condition = "new"
+  end
 
-
-
+  def cobble
+    @cobble
+    puts "Your shoe is as good as new!"
+  end
 end
