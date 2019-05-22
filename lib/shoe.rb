f17b9611a6bf0558b9c9f76fe0f84941643f974a
Denali Balser <denali.balser167@gmail.com>
2019-05-22 00:43:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,10 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
   end
 
-  def condition=(condition)
-    @condition = condition
-  end
 
-  def condition
-    @condition
-  end
 
 end
